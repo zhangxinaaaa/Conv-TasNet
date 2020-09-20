4622d93d0b9dbe23584addd4f4b9463255651652
zhangxinaaaa <1670225598@qq.com>
2020-09-20 08:29:25 +0800

    123

--- test/learn_conv1d.py
+++ test/learn_conv1d.py
@@ -2,14 +2,15 @@
 # Author: Kaituo XU
 
 import torch
+
 torch.manual_seed(123)
 
 Cin, Cout, F, S = 4, 3, 2, 1
 B, Lin = 2, 5
 D = 2
 print('Cin, Cout, F, S, B, Lin, D=', Cin, Cout, F, S, B, Lin, D)
-conv1d = torch.nn.Conv1d(Cin, Cin, F, S, bias=False, padding=(F-1)*D, dilation=D, groups=Cin)
-#conv1d = torch.nn.Conv1d(Cin, Cin, F, S, bias=False, padding=1, dilation=1, groups=Cin)
+conv1d = torch.nn.Conv1d(Cin, Cin, F, S, bias=False, padding=(F - 1) * D, dilation=D, groups=Cin)
+# conv1d = torch.nn.Conv1d(Cin, Cin, F, S, bias=False, padding=1, dilation=1, groups=Cin)
 inputs = torch.randint(3, (B, Cin, Lin))
 conv1d.weight.data = torch.randint(5, conv1d.weight.size())
 outputs = conv1d(inputs)
@@ -22,7 +23,7 @@ print('outputs', outputs.size())
 print('inputs\n', inputs)
 print('weight\n', conv1d.weight)
 print('outputs\n', outputs)
-print('chomp outputs\n', outputs[:,:,:-(F-1)*D])
+print('chomp outputs\n', outputs[:, :, :-(F - 1) * D])
 
 # m = torch.nn.Conv1d(16, 33, 3, stride=2)
 # print(m.weight.size())
